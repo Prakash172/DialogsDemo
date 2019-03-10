250470624b097963a80d211798f31daf56834106
Prakash172 <electrophile172@gmail.com>
2019-03-10 19:02:38 +0530

    DialogFragment is displayed successfully

---
+++ app/src/main/java/com/tvs/dialogsdemo/InputDialogFragment.java
@@ -0,0 +1,50 @@
+package com.tvs.dialogsdemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class InputDialogFragment extends DialogFragment {
+
+    private static InputDialogFragment inputDialogFragment = null;
+    private int n;
+
+    public static InputDialogFragment newInstance() {
+        if (inputDialogFragment == null) {
+            inputDialogFragment = new InputDialogFragment();
+        }
+        return inputDialogFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.input_dialog_layout, container, false);
+        final EditText editText = view.findViewById(R.id.editText);
+
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(editText.getText().toString())) {
+                    n = Integer.valueOf(editText.getText().toString());
+                    Toast.makeText(getActivity(), ""+n+" is entered", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getActivity(),"Please enter non zero number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/tvs/dialogsdemo/MainActivity.java
+++ app/src/main/java/com/tvs/dialogsdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tvs.dialogsdemo;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showInputDialog();
+    }
+
+    public void showInputDialog(){
+        InputDialogFragment inputDialogFragment = InputDialogFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        inputDialogFragment.show(fragmentTransaction,"input-fragment-dialog");
     }
 }
+
+/*
+* 1.Dialogs are based on fragment from android 3.0 and it is recommended to use fragments dialog not activity
+* 2.Dialogs are asynchronous
+* 3. Steps for the dialog fragment
+*   > Create a dialog fragment
+*   > Get a fragment transaction
+*   > Show the dialog using the fragment transaction from step 2
+* */
